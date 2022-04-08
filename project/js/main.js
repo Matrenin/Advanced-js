9039420462508d15d39495952af0deb59ac19ca4
Roman <generallevy45@mail.ru>
2022-04-08 21:50:43 +0300

    add hw-1

--- project/js/main.js
+++ project/js/main.js
@@ -5,21 +5,44 @@ const products = [
     {id: 4, title: 'Gamepad', price: 150},
 ];
 
-const renderProduct = (title, price) => {
+const divEl = document.querySelector(".products");
+
+const renderProduct = (title, price, img) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
                 <p>${price}</p>
                 <button class="by-btn">Добавить</button>
-              </div>`;
+            </div>`;
 };
 
-const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    document.querySelector('.products').innerHTML = productList;
+// const renderProducts = (list) => {
+//     const productList = list.map((good) => {
+//         return renderProduct(good.title, good.price);
+//     });
+//     divEl.innerHTML = productList;
 
-    console.log(productList);
-};
+//     console.log(productList);
+// };
 
-renderProducts(products);
+// renderProducts(products);
+
+const productList = products.map((good) => {
+    return renderProduct(good.title, good.price, good.img);
+});
+
+divEl.insertAdjacentHTML("beforeend", productList);
+
+// 1.
+divEl.classList.add("parent-products");
+document.querySelector(".btn-cart").classList.add("new-btn");
+document.querySelectorAll(".product-item").forEach(el => el.classList.add("new-productItem"));
+document.querySelectorAll("h3").forEach(el => el.classList.add("product-title"));
+document.querySelectorAll(".products button").forEach(el => el.classList.add("product-btn"));
+
+// 3.
+const divNodes = divEl.childNodes;
+divNodes.forEach(node => {
+    if (node.nodeName == "#text") {
+        node.remove();
+    }
+});
