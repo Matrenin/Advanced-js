dbf629abe21689a464efb25619f700cd34dc0e37
Roman <generallevy45@mail.ru>
2022-04-12 10:10:17 +0300

    add hw-2

--- project/js/main.js
+++ project/js/main.js
@@ -1,25 +1,51 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 1000},
-    {id: 2, title: 'Mouse', price: 100},
-    {id: 3, title: 'Keyboard', price: 250},
-    {id: 4, title: 'Gamepad', price: 150},
-];
+class ProductList {
+    constructor(container = '.products') {
+        this.container = document.querySelector(container);
+        this._goods = [];
+        this._productsObjects = [];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить</button>
+        this._fetchGoods();
+        this._render();
+    }
+
+    _fetchGoods() {
+        this._goods = [
+            {id: 1, title: 'Notebook', price: 20000},
+            {id: 2, title: 'Mouse', price: 1500},
+            {id: 3, title: 'Keyboard', price: 5000},
+            {id: 4, title: 'Gamepad', price: 4500},
+        ];
+    }
+
+    _render() {
+        for (const product of this._goods) {
+            const productObject = new ProductItem(product);
+            console.log(productObject);
+
+            this._productsObjects.push(productObject);
+            this.container.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+        }
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.id = product.id;
+        this.title = product.title;
+        this.price = product.price;
+        this.img = img;
+    }
+
+    getHTMLString() {
+        return `<div class="product-item" data-id="${this.id}">
+                  <img src="${this.img}" alt="Some img">
+                  <div class="desc">
+                      <h3>${this.title}</h3>
+                      <p>${this.price} \u20bd</p>
+                      <button class="buy-btn">Купить</button>
+                  </div>
               </div>`;
-};
+    }
+}
 
-const renderProducts = (list) => {
-    const productList = list.map((good) => {
-        return renderProduct(good.title, good.price);
-    });
-    document.querySelector('.products').innerHTML = productList;
-
-    console.log(productList);
-};
-
-renderProducts(products);
+const list = new ProductList();
